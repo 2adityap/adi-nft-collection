4ddc2ae8b55ad5ba5b411346be82b89f2d760cba
Aditya <ap5pp@virginia.edu>
2022-01-26 13:24:59 -0500

    first commits

---
+++ app/src/App.js
@@ -0,0 +1,101 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = 'AdityaPenmesta';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  //State
+  const [walletAddress, setWalletAddress] = useState(null);
+
+
+  //Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+      /*
+       * The solana object gives us a function that will allow us to connect
+       * directly with the user's wallet!
+       */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+            alert('Solana object not found! Please create a Phantom Wallet 👻');
+          }
+      }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana ) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+// If user hasn't connected their wallet yet, then run this
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    > Connect to Wallet </button>
+  );
+
+
+  /*
+      Check if user is already authenticated
+  */
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+          <p className="sub-text">NFT Marketplace for Creators and Fans</p>
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
